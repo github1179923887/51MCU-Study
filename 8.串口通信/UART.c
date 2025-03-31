7be3d44f1e4016465f98bc3767a7b99a9b0b47a0
oo <1179923887@qq.com>
2025-03-31 14:38:08 +0800

    完成串口收发数据并根据接收数据控制LED

---
+++ 8.串口通信/UART.c
@@ -0,0 +1,54 @@
+#include <REGX52.H>
+
+/**
+	*  @breif	4800bps@11.0592MHz串口初始化
+	*  @param	无
+	*  @retval	无
+	*/
+void UART_Init()	//4800bps@11.0592MHz
+{
+	//配置串口相关寄存器
+	SCON = 0x50;	//0100 0000 分别指SM0,SM1,SM2,REN,TB8,RB8,TI,RI，具体功能见手册P235，1：0x40未打开接收，2：0x50打开接收（即REN置1）
+	PCON |= 0x80;
+	
+	//配置定时器1
+	TMOD &= 0x0F;		//设置定时器模式，模式2：八位自动重装模式
+	TMOD |= 0x20;		//设置定时器模式
+	TL1 = 0xF4;		//设定定时初值
+	TH1 = 0xF4;		//设定定时器重装值
+	ET1 = 0;		//禁止定时器1中断
+	TR1 = 1;		//启动定时器1
+	
+	//配置中断，需要接收时，优先级取默认
+	ES = 1;
+	EA = 1;
+}
+/**
+	*  @breif	串口发送一个字节数据
+	*  @param	Byte 要发送的数据
+	*  @retval	无
+	*/
+void UART_SendByte(unsigned char Byte)
+{
+	SBUF = Byte;	//串口数据缓存寄存器
+	while(TI == 0);	//检测到TI==1(即数据发送完成)时跳出循环，将TI置0
+	TI = 0;
+}
+/**
+	*  @breif	串口中断函数模板
+	*  @param	无
+	*  @retval	无
+	*/
+/*
+void UART_Routine() interrupt 4	//串口中断号
+{
+	if(RI == 1)	//数据接收完成，作判断的原因是发送和接收公用一个中断口，需要区分
+	{
+		
+		RI = 0;
+	}
+}
+*/
+
+
+
---
+++ 8.串口通信/main.c
@@ -0,0 +1,53 @@
+//向电脑发送数据，注意波特率对齐
+//#include <REGX52.H>
+//#include "Delay.h"
+//#include "UART.h"
+
+//unsigned char Sec;
+
+//void main()
+//{
+//	UART_Init();
+//	while(1)
+//	{
+//		UART_SendByte(Sec);	//串口助手内接收的数据以16进制显示
+//		Sec++;
+//		Delayxms(1000);	//否则另一端接收到的数据不对，延时1ms即可
+//	}
+//}
+
+//电脑通过串口控制LED
+#include <REGX52.H>
+#include "Delay.h"
+#include "UART.h"
+
+unsigned char Sec;
+
+void main()
+{
+	UART_Init();
+	while(1)
+	{
+		//以下等价，实际存储的值是一样的
+		//UART_SendByte('B');
+		//UART_SendByte(0x42);
+		
+		//将中断配置断开即（ES=0或EA=0），以下方法也可实现，可能是中断连通时当数据接收完成会自动调用中断函数，数据发送则不会
+//		while(RI == 0);
+//		P2 = ~SBUF;	//读取另一端发送的数据控制LED
+//		UART_SendByte(SBUF);	//将接收到的数据发送回去
+//		RI = 0;
+	}
+}
+
+void UART_Routine() interrupt 4	//串口中断号
+{
+	if(RI == 1)	//数据接收完成，作判断的原因是发送和接收公用一个中断口，需要区分
+	{
+		P2 = ~SBUF;	//读取另一端发送的数据控制LED
+		UART_SendByte(SBUF);	//将接收到的数据发送回去
+		RI = 0;
+	}
+}
+
+//串口助手内的文本模式与HEX(二进制、八进制、十六进制)模式之间的转换是通过ASCLL码一一对应，输出字母时需要用到该知识点
