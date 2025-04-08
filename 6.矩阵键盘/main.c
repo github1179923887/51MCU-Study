cc969750e182acaaa04fc9dae8dd5b0a9770ef19
laptop-yezi <1179923887@qq.com>
2025-04-08 21:18:46 +0800

    完善注释和排版

--- 6.矩阵键盘/main.c
+++ 6.矩阵键盘/main.c
@@ -8,13 +8,6 @@
 
 //void main()
 //{
-////	LCD_Init();
-////	LCD_ShowChar(1,1,'A');
-////	LCD_ShowString(1,3,"Hello");
-////	LCD_ShowNum(1,9,123,3);
-////	LCD_ShowSignedNum(1,13,-66,2);
-////	LCD_ShowHexNum(2,1,0xA8,2);	//十六进制
-////	LCD_ShowBinNum(2,4,0xAA,8);	//二进制
 //	LCD_Init();
 //	LCD_ShowString(1,1,"InputValue:");
 //	while(1)
