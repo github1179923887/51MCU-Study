0401fc2d10d80fd49f6e2ea9246f790cd6495cc3
oo <1179923887@qq.com>
2025-04-08 10:22:50 +0800

    修改错误

--- 20.红外遥控控制电机调速/main.c
+++ 20.红外遥控控制电机调速/main.c
@@ -21,9 +21,7 @@ void main()
 			if(Command == IR_1){Speed = 1;}
 			if(Command == IR_2){Speed = 2;}
 			if(Command == IR_3){Speed = 3;}
-			
-			Speed++;
-			Speed%=4;
+
 			if(Speed == 0){Motor_SetSpeed(0);}
 			if(Speed == 1){Motor_SetSpeed(50);}
 			if(Speed == 2){Motor_SetSpeed(75);}
